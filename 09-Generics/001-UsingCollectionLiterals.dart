6070aadfdfdce050108821031e60d1a33a5714e7
wk-j <somnuk.wk@gmail.com>
2019-08-17 18:09:29 +0700

    Refactor

---
+++ 09-Generics/001-UsingCollectionLiterals.dart
@@ -0,0 +1,9 @@
+main() {
+  var name = <String>["Seth", "Kathy", "Lars"];
+  var uniqeNames = <String>{"Seth", "Kathy", "Lars"};
+  var pages = <String, String>{
+    "index.html": "Homepage",
+    "robot.txt": "Hints for web remotes",
+    "humans.txt": "We are poepole, not machines"
+  };
+}
